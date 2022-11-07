61e1f9fb1c029694c529e67802db00f4bcd3d5a8
conradjohnson <skaterjames@gmail.com>
2022-11-07 14:35:52 -0600

    trying to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//process.env.NODE_ENV = 'production';
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
