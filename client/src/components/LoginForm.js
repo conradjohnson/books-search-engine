b7269aad518cb901956dc706925cba9dc06e93af
conradjohnson <skaterjames@gmail.com>
2022-11-06 22:46:09 -0600

    register user now converted

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,11 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -33,7 +33,7 @@ const LoginForm = () => {
       const { data } = await login({
         variables: { ...userFormData },
       });
-      //const response = await loginUser(userFormData);
+      
 
       Auth.login(data.login.token);
     } catch (e) {
@@ -41,14 +41,7 @@ const LoginForm = () => {
       setShowAlert(true);
     }
 
-    //   const { token, user } = await response.json();
-    //   console.log(user);
-    //   Auth.login(token);
-    // } catch (err) {
-    //   console.error(err);
-    //   setShowAlert(true);
-    // }
-
+   
     setUserFormData({
       username: '',
       email: '',
@@ -58,6 +51,7 @@ const LoginForm = () => {
 
   return (
     <>
+     
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your login credentials!
@@ -94,6 +88,12 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+     
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">
+          {error.message}
+        </div>
+      )}
     </>
   );
 };
